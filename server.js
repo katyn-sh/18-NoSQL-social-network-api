15504ce558e337d5dea4c68e504b2a237147e15b
katyn-sh <tanhung.huynh@protonmail.ch>
2023-09-05 20:06:45 -0500

    dayjs

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.json());
+app.use(routes);
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Social Network API server running on port ${PORT}!`);
+  });
 });
-
-mongoose.set('debug', true);
-
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
