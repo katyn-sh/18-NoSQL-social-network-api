c9a2f732045d541ca4512fa3d00aef33d924c33d
katyn-sh <tanhung.huynh@protonmail.ch>
2023-09-05 20:03:25 -0500

    fixed routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,31 +1,21 @@
 const router = require('express').Router();
-
 const {
-    getAllUser,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    deleteFriend
-  } = require('../../controllers/user-controller');
+  getAllUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/users
-router
-  .route('/')
-  .get(getAllUser)
-  .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/:id
-router
-  .route('/:id')
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
+router.route('/').get(getAllUsers).post(createUser);
 
-router
-  .route('/:userId/friends/:friendId')
-  .post(addFriend)
-  .delete(deleteFriend);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+
+router.route('/:userId/friends/:friendsId').put(addFriend).delete(deleteFriend);
 
 module.exports = router;
